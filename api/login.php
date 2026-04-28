491856655e14c9f7b68e36deeb02ea17f0903c67
tatameirzha-cloud <tatameirzha@student.uns.ac.id>
2026-04-28 12:31:06 +0700

    login

--- api/login.php
+++ api/login.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Login Panen</title>
-<link rel="stylesheet" href="./css/Login_Register_form.css">
+<link rel="stylesheet" href="/css/Login_Register_form.css">
 
 </head>
 
